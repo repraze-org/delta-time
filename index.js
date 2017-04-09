2381ef4f97e101697dc788308e046a677a6e91e1
Thomas Dubosc <repraze@gmail.com>
2017-04-09 20:07:34 +0100

    Creates node module with tests

---
+++ index.js
@@ -0,0 +1,50 @@
+'use strict';
+
+var second  = 1000,
+    minute  = second * 60,
+    hour    = minute * 60,
+    day     = hour * 24,
+    week    = day * 7,
+    month   = day * 30.44,
+    year    = day * 365.25;
+
+var units = [
+    {match : ['ms','milli','millis','millisecond','milliseconds'],  val : 1},
+    {match : ['s','sec','secs','second','seconds'],                 val : second},
+    {match : ['m','min','mins','minute','minutes'],                 val : minute},
+    {match : ['h','hr','hrs','hour','hours'],                       val : hour},
+    {match : ['d','day','days'],                                    val : day},
+    {match : ['w','wk','wks','week','weeks'],                       val : week},
+    {match : ['mo','mos','month','months'],                         val : month},
+    {match : ['y','yr','yrs','year','years'],                       val : year},
+];
+
+var getMultipliyer = function(unit){
+    var u = units.find(function(u){
+        return u.match.indexOf(unit) >= 0;
+    });
+    if(u){
+        return u.val;
+    }
+    return 0
+};
+
+module.exports = function(dt){
+    if(!isNaN(+dt)){
+        return +dt;
+    }
+
+    if(!(typeof dt === 'string' || dt instanceof String)){
+        return 0;
+    }
+
+    return dt.match(/\-?\d+\s*[a-z]+/g).reduce(function(acc, val){
+        var infos = val.match(/(-?\d+)[\s]*([^\s]*)/);
+        var amount = parseInt(infos[1], 10);
+        var unit = infos[2].toLowerCase();
+
+        var multipliyer = getMultipliyer(unit);
+
+        return acc + amount * multipliyer;
+    }, 0);
+}
