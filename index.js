7b6d4708a8f2968e1e3767df52fd673c29132b32
repraze <repraze@gmail.com>
2017-06-11 18:36:08 +0100

    IFI the unit map

--- index.js
+++ index.js
@@ -10,32 +10,29 @@ var nanosecond  = 1e-6,
     month       = day * 30.44,
     year        = day * 365.25;
 
-var units = [
-    {match : ['ms','milli','millis','millisecond','milliseconds'],  val : 1},
-    {match : ['s','sec','secs','second','seconds'],                 val : second},
-    {match : ['m','min','mins','minute','minutes'],                 val : minute},
-    {match : ['h','hr','hrs','hour','hours'],                       val : hour},
-    {match : ['d','day','days'],                                    val : day},
-    {match : ['w','wk','wks','week','weeks'],                       val : week},
-    {match : ['mo','mos','month','months'],                         val : month},
-    {match : ['y','yr','yrs','year','years'],                       val : year},
-    {match : ['μs','micro','micros','microsecond','microseconds'],  val : microsecond},
-    {match : ['ns','nano','nanos','nanosecond','nanoseconds'],      val : nanosecond}
-];
+var units = (function(){
+    return [
+        {match : ['ms','milli','millis','millisecond','milliseconds'],  val : 1},
+        {match : ['s','sec','secs','second','seconds'],                 val : second},
+        {match : ['m','min','mins','minute','minutes'],                 val : minute},
+        {match : ['h','hr','hrs','hour','hours'],                       val : hour},
+        {match : ['d','day','days'],                                    val : day},
+        {match : ['w','wk','wks','week','weeks'],                       val : week},
+        {match : ['mo','mos','month','months'],                         val : month},
+        {match : ['y','yr','yrs','year','years'],                       val : year},
+        {match : ['μs','micro','micros','microsecond','microseconds'],  val : microsecond},
+        {match : ['ns','nano','nanos','nanosecond','nanoseconds'],      val : nanosecond}
+    ].reduce(function(units, u){
+        u.match.forEach(function(name){
+            units[name] = u.val;
+        });
+        return units;
+    }, {});
+}());
 
 var match   = /(\-?\s*(\.\d+|\d+(\.\d*)?))\s*[a-zμ]+/g;
 var split   = /(-?[^a-zμ\s]+)([^\s]+)/;
 var strip   = /\s+/g;
-
-var getMultipliyer = function(unit){
-    var u = units.find(function(u){
-        return u.match.indexOf(unit) >= 0;
-    });
-    if(u){
-        return u.val;
-    }
-    return 0
-};
 
 var isString = function(s){
     return typeof s === 'string' || s instanceof String;
@@ -44,7 +41,7 @@ var isString = function(s){
 module.exports = function(dt, unit){
     var divider = 1;
     if(unit != undefined){
-        divider = isString(unit) ? getMultipliyer(unit.toLowerCase()) : null;
+        divider = isString(unit) ? units[unit.toLowerCase()] : null;
         if(!divider){
             throw new Error("Unit given as a second parameter is invalid");
         }
@@ -65,7 +62,7 @@ module.exports = function(dt, unit){
             var amount = parseFloat(infos[1], 10);
             var unit = infos[2].toLowerCase();
 
-            var multipliyer = getMultipliyer(unit);
+            var multipliyer = units[unit] || 0;
 
             return acc + amount * multipliyer;
         }, 0) / divider;
